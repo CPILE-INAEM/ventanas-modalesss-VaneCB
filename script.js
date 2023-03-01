9b7b3325bc0d3bc04ee16589135a9aa5fcdbdf68
vanesa <vcarrerabcn@gmail.com>
2023-03-01 13:06:29 +0100

    Funcionalidad de abrir las modales y ocultarlas con un click

---
+++ script.js
@@ -0,0 +1,25 @@
+const overlay = document.querySelector('.overlay')
+const closeModal = document.querySelector('.close-modal')
+const modal = document.querySelector('.modal')
+const showModal = document.querySelectorAll('.show-modal')
+
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModal)
+  console.log(showModal)
+}
+
+//Lo mismo que con el for pero con un foreach
+//showModal.forEach((btn)=> btn.addEventListener('click,openModal'))
+console.log(showModal)
+const closedModal = function () {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+
+closeModal.addEventListener('click', closedModal)
+overlay.addEventListener('click', closedModal)
